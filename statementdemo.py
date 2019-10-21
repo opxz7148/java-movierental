d1e448c60040a29a33d7a2d06d86792fe3bb4814
jbrucker <jebrucker@gmail.com>
2019-10-21 19:04:01 +0700

    update statementdemo

--- statementdemo.py
+++ statementdemo.py
@@ -4,16 +4,16 @@ from customer import Customer
 
 def make_movies():
 	movies = [
+		Movie("The Irishman", Movie.NEW_RELEASE),
 		Movie("CitizenFour", Movie.REGULAR),
 		Movie("Frozen", Movie.CHILDRENS),
 		Movie("El Camino", Movie.NEW_RELEASE),
-		Movie("Particle Fever", Movie.REGULAR),
-		Movie("The Irishman", 4)
+		Movie("Particle Fever", Movie.REGULAR)
 		]
 	return movies
 
 def create_customer_and_rental():
-	customer = Customer("Movie Maniac")
+	customer = Customer("Edward Snowden")
 	days = 1
 	for movie in make_movies():
 		customer.addRental(Rental(movie,days))
@@ -21,7 +21,7 @@ def create_customer_and_rental():
 	return customer
 
 def print_report(customer: Customer):
-	print( customer.createStatement())
+	print( customer.createStatement() )
 
 if __name__ == '__main__':
 	customer = create_customer_and_rental()
