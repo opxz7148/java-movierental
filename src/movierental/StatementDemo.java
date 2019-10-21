985bba999c17b09ab4af84941a8cc8f5358c1a76
jbrucker <jebrucker@gmail.com>
2019-10-21 18:58:45 +0700

    change the movies

--- src/movierental/StatementDemo.java
+++ src/movierental/StatementDemo.java
@@ -6,14 +6,13 @@ package movierental;
 public class StatementDemo {
 	public static void main(String[] args) {
 		Movie[] movies = new Movie[] {
+				new Movie("Bridge of Spies", Movie.NEW_RELEASE),
 				new Movie("CitizenFour", Movie.REGULAR),
 				new Movie("Frozen", Movie.CHILDRENS),
-				new Movie("Ex Machina", Movie.NEW_RELEASE),
-				new Movie("Bridge of Spies", Movie.NEW_RELEASE),
+				new Movie("El Camino", Movie.NEW_RELEASE),
 				new Movie("Particle Fever", Movie.REGULAR)
 		};
-		Customer cust = new Customer("Movie Maniac");
-		int days = 1;
+		Customer cust = new Customer("Edwin Snowden");
 		for(Movie m: movies) {
 			cust.addRental(new Rental(m, days));
 			days++;
