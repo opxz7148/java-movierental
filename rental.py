e06d8d7ee9a2ec4530d4af423a53abfbec0bec49
jbrucker <jebrucker@gmail.com>
2022-10-18 13:46:02 +0700

    replace tabs with spaces

--- rental.py
+++ rental.py
@@ -1,23 +1,23 @@
 class Rental:
-	"""
-	A rental of a movie by customer.
-	From Fowler's refactoring example.
+    """
+    A rental of a movie by customer.
+    From Fowler's refactoring example.
 
-	A realistic Rental would have fields for the dates
-	that the movie was rented and returned, from which the
-	rental period is calculated.
-	For simplicity of this application only days_rented is recorded.
-	"""
-	
-	def __init__(self, movie, days_rented): 
-		"""Initialize a new movie rental object for
-		   a movie with known rental period (daysRented).
-		"""
-		self.movie = movie
-		self.days_rented = days_rented
+    A realistic Rental would have fields for the dates
+    that the movie was rented and returned, from which the
+    rental period is calculated.
+    For simplicity of this application only days_rented is recorded.
+    """
+    
+    def __init__(self, movie, days_rented): 
+    	"""Initialize a new movie rental object for
+    	   a movie with known rental period (daysRented).
+    	"""
+    	self.movie = movie
+    	self.days_rented = days_rented
 
-	def get_movie(self):
-		return self.movie
+    def get_movie(self):
+    	return self.movie
 
-	def get_days_rented(self):
-		return self.days_rented
+    def get_days_rented(self):
+    	return self.days_rented
